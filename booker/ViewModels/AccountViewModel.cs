51d76ae72f606d062091b81df6c0b7e9178695d3
al3xStark <starkov.al3xandr@gmail.com>
2023-09-14 01:22:07 +0300

    switch to abstract AccountViewModel

---
+++ booker/ViewModels/AccountViewModel.cs
@@ -0,0 +1,43 @@
+﻿using booker.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+
+namespace booker.ViewModels
+{
+    public abstract class AccountViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected IAccount account;
+        public AccountViewModel(IAccount account) => this.account = account;
+        public string Title
+        {
+            get => account.Title;
+            set
+            {
+                if (account.Title != value)
+                {
+                    account.Title = value;
+                    OnPropertyChanged("Title");
+                }
+            }
+        }
+        public int Balance
+        {
+            get => account.Balance;
+            set
+            {
+                if (account.Balance != value)
+                {
+                    account.Balance = value;
+                    OnPropertyChanged("Balance");
+                }
+            }
+        }
+        protected void OnPropertyChanged(string propName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propName));
+        }
+    }
+}
--- booker/ViewModels/ComplexAcccountViewModel.cs
+++ booker/ViewModels/ComplexAcccountViewModel.cs
@@ -7,43 +7,9 @@ using static Xamarin.Essentials.Permissions;
 
 namespace booker.ViewModels
 {
-    public class ComplexAcccountViewModel : IAccountViewModel
+    public class ComplexAcccountViewModel : AccountViewModel
     {
-        public event PropertyChangedEventHandler PropertyChanged;
-        private IAccount complexAccount;
-
-        public ComplexAcccountViewModel()
-        {
-            complexAccount = new ComplexAccount();
-        }
-        public string Title
-        {
-            get => complexAccount.Title;
-            set
-            {
-                if (complexAccount.Title != value)
-                {
-                    complexAccount.Title = value;
-                    OnPropertyChanged("Title");
-                }
-            }
-        }
-        public int Balance
-        {
-            get => complexAccount.Balance;
-            set
-            {
-                if (complexAccount.Balance != value)
-                {
-                    complexAccount.Balance = value;
-                    OnPropertyChanged("Balance");
-                }
-            }
-        }
-        public Segment[] Segments => ((ComplexAccount)complexAccount).Segments;
-        protected void OnPropertyChanged(string propName)
-        {
-            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propName));
-        }
+        public ComplexAcccountViewModel() : base(new ComplexAccount()) { }
+        public Segment[] Segments => ((ComplexAccount)account).Segments;
     }
 }
